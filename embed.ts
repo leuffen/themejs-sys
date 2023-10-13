cabb5ea9078589d2268d84209bbc569e05188ebf
Matthias Leuffen <matthes@leuffen.de>
2023-10-14 01:44:07 +0200

    update

--- embed.ts
+++ embed.ts
@@ -1,3 +1,4 @@
-import {initLoader} from "@leuffen/liscom/loader";
-
+import {initLoader} from "@leuffen/liscom-loader";
+import {MicxFormmailFacade} from "@micx/lib-js";
+(new MicxFormmailFacade).observe();
 initLoader()
