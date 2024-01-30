a53db35375cba8df7f5775948bf93f800143ed4f
Marcel Wasilewski <marcel@wasilewski.io>
2024-01-30 23:46:55 +0100

    adjust styles for final website

--- sections/contact-form/contact-form.ts
+++ sections/contact-form/contact-form.ts
@@ -15,23 +15,6 @@ Joda.registerTemplate("contact-form",
                         <div class="tjs-contact-form__col tjs-contact-form__form">
                             <form data-micx-formmail-preset="default" data-micx-formmail-sent-message="E-Mail erfolgreich gesendet!">
                                 <slot data-select=".form, form"></slot>
-                                <div class="tjs-contact__newsletter--callback-wrapper">
-                                    <span>Kontaktieren Sie mich per:</span>
-                                    <div>
-                                        <div class="form-check">
-                                            <input class="form-check-input" type="radio" name="contact-input-callback-type" id="contact-input-callback-type-phone">
-                                            <label class="form-check-label" for="contact-input-callback-type-phone">
-                                                Telefon
-                                            </label>
-                                        </div>
-                                        <div class="form-check">
-                                            <input class="form-check-input" type="radio" name="contact-input-callback-type" id="contact-input-callback-type-email" checked>
-                                            <label class="form-check-label" for="contact-input-callback-type-email">
-                                                E-Mail
-                                            </label>
-                                        </div>
-                                    </div>
-                                </div>
                             </form>
                         </div>
                     </div>
